97256721afadea65c4546640d474099b89808885
HarrisonLima <harrisonlima92@gmail.com>
2026-06-22 21:04:15 -0300

    Created prediction function. Returned category probality

--- demos/exemplo-00/index.js
+++ demos/exemplo-00/index.js
@@ -13,7 +13,7 @@ async function trainModel(inputXs, outputYs) {
   // A ReLU age como um filtro:
   // É como se ela deixasse somente os dados interessantes seguirem viagem na rede
   // Se a informação chegou nesse neurônio é positiva, passa para frente!
-  // Se for zero ou negativa, pode jogar fora,nãovai servir
+  // Se for zero ou negativa, pode jogar fora, não vai servir
   model.add(
     tf.layers.dense({ inputShape: [7], units: 80, activation: "relu" }),
   );
@@ -47,18 +47,28 @@ async function trainModel(inputXs, outputYs) {
   // Treinamento do modelo
   // verbose: desabilita o log interno (e usa só callback)
   // epochs: quantiadde de vezes que vai rodar no dataset
-  // shuffle: embaralha os dadosm para evitar viés
+  // shuffle: embaralha os dados para evitar viés
   await model.fit(inputXs, outputYs, {
     verbose: 0,
     epochs: 100,
     shuffle: true,
-    callbacks: {
-      onEpochEnd: (epoch, log) =>
-        console.log(`Epoch: ${epoch}: loss = ${log.loss}`),
-    },
+    // callbacks: {
+    //   onEpochEnd: (epoch, log) =>
+    //     console.log(`Epoch: ${epoch}: loss = ${log.loss}`),
+    // },
   });
 
   return model;
+}
+
+async function predict(model, pessoa) {
+  // transformar o array js para o tensor (tfjs)
+  const tfInput = tf.tensor2d(pessoa);
+
+  // Faz a predição (output será um vetor de 3 probabilidades)
+  const pred = model.predict(tfInput);
+  const predArray = await pred.array();
+  return predArray[0].map((prob, index) => ({ prob, index }));
 }
 
 // Exemplo de pessoas para treino (cada pessoa com idade, cor e localização)
@@ -100,4 +110,28 @@ const outputYs = tf.tensor2d(tensorLabels);
 
 // quanto mais dados melhor!
 // assim o algoritmo consegue entender melhor os padrões complexos dos dados
-const models = trainModel(inputXs, outputYs);
+const model = await trainModel(inputXs, outputYs);
+
+const pessoa = { nome: "zé", idade: 28, cor: "verde", localização: "Curitiba" };
+// Normalizando a idade da nova pessoa usando o mesmo padrão do treino
+// Exemplo: idade_min = 25, idade_max = 40, então (28 - 25) / (40 - 25) = 0.2
+
+const pessoaTensorNormalizada = [
+  [
+    0.2, // idade normalizada
+    1, // cor azul
+    0, // cor vermelho
+    0, // cor verde
+    1, // lozalização São Paulo
+    0, // localização Rio
+    0, // localização Curitiba
+  ],
+];
+
+const predictions = await predict(model, pessoaTensorNormalizada);
+const results = predictions
+  .sort((a, b) => b.prob - a.prob)
+  .map((p) => `${labelsNomes[p.index]} (${(p.prob * 100).toFixed(2)}%)`)
+  .join("\n");
+
+console.log(results);
